555925b59a3709f9c04a2a226d9684907c92aea2
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-03 15:27:18 -0500

    changed old table html to updated table html, changed for loop to put order from acending to decending

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
@@ -12,7 +12,7 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i<coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
