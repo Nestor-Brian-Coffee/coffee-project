8dbd31107e35beb9d88531ea586c0af3c8cdc185
Brian Torluemke <briantorluemke@gmail.com>
2018-04-04 12:26:57 -0500

    Made functions more FUNCTIONAL.

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 "use strict";
 
-// function renderCoffee(coffee) {
-//     var html = '<tr class="coffee">';
-//     html += '<td>' + coffee.id + '</td>';
-//     html += '<td>' + coffee.name + '</td>';
-//     html += '<td>' + coffee.roast + '</td>';
-//     html += '</tr>';
-//
-//     return html;
-// }
+function renderCoffee(coffee) {
+    var coffeeClass;
+    if (coffee.roast === "dark") {
+        coffeeClass = "drk-card"
+    } if (coffee.roast === "medium") {
+        coffeeClass = "med-card"
+    }if (coffee.roast === "light") {
+        coffeeClass = "lt-card"
+    }
+    var html = '<div class="card col-3 mr-3 mt-3 coffeeCards ' + coffeeClass + '">';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+    return html;
+}
+
+
 
 function renderCoffees(coffees) {
     var html = '';
-    for (var i = 0; i < coffees.length; i++) {
+    for(var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
+
+
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
@@ -27,7 +37,7 @@ function updateCoffees(e) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -48,31 +58,11 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'}
 ];
 
+
+
 var tbody = document.querySelector('#coffees');
+tbody.innerHTML = renderCoffees(coffees);
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-var temp;
-var temp2;
-for (var i = 0; i < coffees.length; i++) {
-    temp = document.createElement("div");
-    temp.className = "card col-3 mr-3 mt-3 coffeeCards";
-    // temp.classList.add('new-class');
-    temp.innerHTML = "<h3>" + coffees[i].name + "</h3>";
-    document.getElementsByClassName("row")[0].appendChild(temp);
-    temp2 = document.createElement("p");
-    temp2.className = "card-text";
-    temp2.style.textAlign = "center";
-    temp2.innerHTML = coffees[i].roast;
-    document.getElementsByClassName("card")[i].appendChild(temp2);
-    if (coffees[i].roast === "dark") {
-        temp.classList.add("drk-card")
-    }
-    if (coffees[i].roast === "medium"){
-        temp.classList.add("med-card")
-    }
-    if (coffees[i].roast === "light"){
-        temp.classList.add("lt-card")
-    }
-}
-// tbody.innerHTML = renderCoffees(coffees);
+
     submitButton.addEventListener('click', updateCoffees);
