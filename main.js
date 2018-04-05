f4c9400e4ecf371f4a94d21a8057ee4b157a0305
nestor salinas <nestorsalinas88@yahoo.com>
2018-04-05 09:54:52 -0500

    added functionality to create new coffee beans

--- main.js
+++ main.js
@@ -41,68 +41,62 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+var coffees;
+if(localStorage.getItem("coffees") !== "null"){
+    coffees = JSON.parse(localStorage.getItem('coffees'));
 
+    // localStorage.getItem("coffees");
+} else{
+     coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'}
+    ];
+
+}
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'}
-];
 
-function createCoffee(coffees, newcoffe, roast) {
-    var roast = (roast !== undefined) ? roast : document.querySelector("#type-roast").value;
-    var newcoffe = (newcoffe !== undefined) ? newcoffe : document.querySelector("#createNewBean").value;
+function createCoffee(e) {
+    e.preventDefault();
+    var roast = document.querySelector("#type-roast").value;
+    var newcoffe = document.querySelector("#new-bean").value;
     var id = coffees.length+1;
 
     var coffee = {
         id: id,
-        name: name,
+        name: newcoffe,
         roast: roast
     };
 
     coffees.push(coffee);
-    updateStoredCoffee(coffees)
+    updateCoffees(e);
+    updateStoredCoffee(coffees);
 
 }
 function updateStoredCoffee(coffees) {
     var string = JSON.stringify(coffees);
     localStorage.setItem('coffees', string);
-
 }
-// function updateCoffees(e) {
-//     e.preventDefault(); // don't submit the form, we just want to update the data
-//     var selectedRoast = roastSelection.value;
-//     var filteredCoffees = [];
-//
-//     coffees.forEach(function (coffee) {
-//         var search = document.querySelector("#search-field").value.toLowerCase();
-//         if ((coffee.roast === selectedRoast || selectedRoast === "All") && coffee.name.toLowerCase().includes(search)=== true) {
-//             filteredCoffees.push(coffee);
-//         }
-//     });
-//     tbody.innerHTML = renderCoffees(filteredCoffees);
-// }
+
 var tbody = document.querySelector('#coffees');
 var roastSelection = document.querySelector('#roast-selection');
 var searchField = document.querySelector("#search-field");
-var roastNew = document.querySelector('#type-roast');
-var coffee = document.querySelector('#new-bean');
+
 var resetCoffe = document.querySelector("#button");
 
 
 tbody.innerHTML = renderCoffees(coffees);
 roastSelection.addEventListener('change', updateCoffees);
 searchField.addEventListener('input' , updateCoffees);
-roastNew.addEventListener('change', createCoffee);
-coffee.addEventListener('keydown', createCoffee);
+resetCoffe.addEventListener('click', createCoffee);
